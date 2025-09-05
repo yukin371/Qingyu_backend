3839165695055573f66fd9288cd11d2705d8eb6d
Yukin <yukin371@outlook.com>
2025-09-05 16:02:33 +0800

    feat: 添加文档管理API和服务层
    - 实现了文档的创建、获取、更新、删除和列表功能
    - 添加了文档数据模型，包含时间戳处理
    - 更新路由以支持新的文档API
    - 引入服务层以处理文档相关的业务逻辑

--- models/document/document.go
+++ models/document/document.go
@@ -1,1 +1,27 @@
 package document
+
+import "time"
+
+// Document 表示系统中的文档数据模型
+// 仅包含与数据本身紧密相关的字段与方法
+type Document struct {
+	ID        string    `bson:"_id,omitempty" json:"id"`
+	UserID    string    `bson:"user_id" json:"userId"`
+	Title     string    `bson:"title" json:"title"`
+	Content   string    `bson:"content" json:"content"`
+	Tags      []string  `bson:"tags" json:"tags"`
+	CreatedAt time.Time `bson:"created_at" json:"createdAt"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updatedAt"`
+}
+
+// TouchForCreate 在创建前设置时间戳
+func (d *Document) TouchForCreate() {
+	now := time.Now()
+	d.CreatedAt = now
+	d.UpdatedAt = now
+}
+
+// TouchForUpdate 在更新前刷新更新时间戳
+func (d *Document) TouchForUpdate() {
+	d.UpdatedAt = time.Now()
+}
--- router/document/doc_document.go
+++ router/document/doc_document.go
@@ -1,6 +1,8 @@
 package document
 
 import (
+	api "Qingyu_backend/api/v1/document"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,19 +11,20 @@ func RegisterRoutes(r *gin.RouterGroup) {
 	// 文档路由组
 	docRouter := r.Group("/document")
 	{
+		a := api.NewDocumentApi()
 		// 创建文档
-		docRouter.POST("/", nil) // TODO: 添加创建文档处理函数
+		docRouter.POST("/", a.Create)
 
 		// 获取文档列表
-		docRouter.GET("/", nil) // TODO: 添加获取文档列表处理函数
+		docRouter.GET("/", a.List)
 
 		// 获取单个文档
-		docRouter.GET("/:id", nil) // TODO: 添加获取单个文档处理函数
+		docRouter.GET("/:id", a.Get)
 
 		// 更新文档
-		docRouter.PUT("/:id", nil) // TODO: 添加更新文档处理函数
+		docRouter.PUT("/:id", a.Update)
 
 		// 删除文档
-		docRouter.DELETE("/:id", nil) // TODO: 添加删除文档处理函数
+		docRouter.DELETE("/:id", a.Delete)
 	}
 }
